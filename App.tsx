5454464a5bc0ae9e9692305a4b4ea6d265226621
Jang Haemin <io@jhaemin.com>
2020-01-18 15:43:35 -0500

    feat: use navigation provider

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavigationNativeContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import FileBrowserScreen from './src/screen/FileBrowserScreen'
 import { enableScreens } from 'react-native-screens'
-import { View, Text } from 'react-native'
+import { getStatusBarHeight } from 'react-native-status-bar-height'
+import { NavigationProvider } from '@src/context/NavigationContext'
 
 enableScreens()
 
@@ -14,36 +15,35 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>()
 
 const App: React.FC = () => {
+  const [statusBarHeight, setStatusBarHeight] = useState(
+    getStatusBarHeight() + 60
+  )
+
   return (
-    <NavigationNativeContainer>
-      <View
-        style={{
-          height: 100
-        }}
-      >
-        <Text>Hello World</Text>
-      </View>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          headerLargeTitle: true,
-          contentStyle: {
-            backgroundColor: '#fff'
-          }
-        }}
-      >
-        <Stack.Screen
-          name="FileBrowser"
-          component={FileBrowserScreen}
-          options={({ route }) => ({
-            title:
-              route.params && route.params.folderName
-                ? route.params.folderName
-                : 'Clowd'
-          })}
-        />
-      </Stack.Navigator>
-    </NavigationNativeContainer>
+    <NavigationProvider>
+      <NavigationNativeContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+            headerLargeTitle: true,
+            contentStyle: {
+              backgroundColor: '#fff'
+            }
+          }}
+        >
+          <Stack.Screen
+            name="FileBrowser"
+            component={FileBrowserScreen}
+            options={({ route }) => ({
+              title:
+                route.params && route.params.folderName
+                  ? route.params.folderName
+                  : 'Clowd'
+            })}
+          />
+        </Stack.Navigator>
+      </NavigationNativeContainer>
+    </NavigationProvider>
   )
 }
 
