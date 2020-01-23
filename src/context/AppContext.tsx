23b52d915a18cd869d453067ce17e1c268500205
Jang Haemin <io@jhaemin.com>
2020-01-22 20:51:04 -0500

    chore: create app context instead of navigation context

---
+++ src/context/AppContext.tsx
@@ -0,0 +1,38 @@
+import React, { createContext, useState } from 'react'
+import { RouteProp } from '@react-navigation/native'
+import { Animated } from 'react-native'
+
+interface AppContextProps {
+  isNavShrunken: boolean
+  setIsNavShrunken: React.Dispatch<React.SetStateAction<boolean>>
+  currentFolderName: string
+  setCurrentFolderName: React.Dispatch<React.SetStateAction<string>>
+  navigation: any
+  setNavigation: React.Dispatch<any>
+  scrollY: Animated.Value
+}
+
+export const AppContext = createContext({} as AppContextProps)
+
+export const AppProvider: React.FC = ({ children }) => {
+  const [isNavShrunken, setIsNavShrunken] = useState(false)
+  const [currentFolderName, setCurrentFolderName] = useState('Clowd')
+  const [navigation, setNavigation] = useState(null)
+  const [scrollY] = useState(new Animated.Value(0))
+
+  return (
+    <AppContext.Provider
+      value={{
+        isNavShrunken,
+        setIsNavShrunken,
+        currentFolderName,
+        setCurrentFolderName,
+        navigation,
+        setNavigation,
+        scrollY
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
