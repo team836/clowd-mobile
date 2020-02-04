83f22e08a1bb8e9682550fa40995df11f4837a74
RyuJaesang <rjs9611@naver.com>
2020-02-04 11:40:42 -0500

    feat: macbook setting

--- src/screen/StartScreen.tsx
+++ src/screen/StartScreen.tsx
@@ -1,64 +1,110 @@
-import React, { Component, useEffect, useState } from 'react'
-import { Image, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from 'react-native'
 
-type GoogleLoginProps = {
-  name: string
-}
+const styles = StyleSheet.create({
+  mainLayout: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'black',
+    flexDirection: 'column',
+  },
 
-type BlinkProps = {
-  text: string
-  isShowingText: boolean
-}
+  clowdLogoLayout: {
+    flex: 5,
+    padding: 10,
+    backgroundColor: 'black',
+    alignItems: 'center',
+  },
 
-const GoogleLogin: React.FC<GoogleLoginProps> = props => {
+  clowdLogoImage: {
+    height: '100%',
+    width: '120%',
+    alignItems: 'center',
+  },
+
+  buttonLayout: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'white',
+  },
+
+  button: {
+    position: 'relative',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttonGoogleLogo_TextLayout: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttonGoogleLogoImage: {
+    width: 50,
+    height: 50,
+  },
+
+  buttonGoogleLogoText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+})
+
+function ClowdLogo() {
   return (
-    <View style={{ alignItems: 'center' }}>
-      <Text style={{ color: 'red' }}>{props.name} Google Button</Text>
+    <View style={styles.clowdLogoLayout}>
+      <Image
+        style={styles.clowdLogoImage}
+        source={require('@src/assets/images/clowd-logo.png')}
+      />
     </View>
   )
 }
 
-class Blink extends Component {
-  componentDidMount() {
-    // Toggle the state every second
-    setInterval(
-      () =>
-        this.setState(previousState => ({
-          isShowingText: !previousState.isShowingText,
-        })),
-      1000
-    )
-  }
-
-  //state object
-  state = { isShowingText: true }
-
-  render() {
-    if (!this.state.isShowingText) {
-      return null
-    }
-
-    return <Text>{this.props.text}</Text>
-  }
+function LogInButton() {
+  return (
+    <View style={styles.buttonLayout}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => Alert.alert('send API')}
+      >
+        <View style={styles.buttonGoogleLogo_TextLayout}>
+          <Image
+            resizeMethod="scale"
+            resizeMode="contain"
+            style={styles.buttonGoogleLogoImage}
+            source={require('@src/assets/images/Google-logo.png')}
+          />
+          <Text style={styles.buttonGoogleLogoText}>Sign in with Google</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
-export default class HelloWorldApp extends Component {
+export default class mainLayout extends Component {
   render() {
-    let pic = {
-      uri:
-        'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg',
-    }
     return (
-      <View style={{ alignItems: 'center', top: 200 }}>
-        <Image
-          source={require('@src/assets/images/clowd-logo.png')}
-          style={{
-            alignItems: 'center',
-            width: 300,
-            height: 300,
-          }}
-        />
-        <GoogleLogin children="GoogleButtonUI" />
+      <View style={styles.mainLayout}>
+        <ClowdLogo />
+        <LogInButton />
       </View>
     )
   }
