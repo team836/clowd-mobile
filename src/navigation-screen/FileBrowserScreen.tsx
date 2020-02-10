507ba0db1532d70121c5480de72bf8fab5a385e7
Jang Haemin <io@jhaemin.com>
2020-02-10 16:42:36 -0500

    feat: test new path structure

--- src/navigation-screen/FileBrowserScreen.tsx
+++ src/navigation-screen/FileBrowserScreen.tsx
@@ -1,20 +1,18 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { StackNavigationProp } from '@react-navigation/stack'
-import { Share, FlatList, Animated, ScrollView } from 'react-native'
+import { Share, Animated } from 'react-native'
 import { RootStackParamList } from '@src/component/MainScaffold'
 import FileBrowserGridItem, {
   ClowdFile,
   ClowdFiles,
 } from '@src/component/FileBrowserGridItem'
 import { getStatusBarHeight } from 'react-native-status-bar-height'
-import ClowdStatusBar from '@src/component/StatusBar'
 import {
   RouteProp,
   useRoute,
   EventListenerCallback,
 } from '@react-navigation/native'
 import { sampleFiles } from '@src/data/sample-files'
-import { BlurView } from 'expo-blur'
 import { ClowdConstants } from '@src/constants'
 import { AppContext } from '@src/context/AppContext'
 import { getBottomSpace } from 'react-native-iphone-x-helper'
@@ -33,52 +31,69 @@ export type FileBrowserScreenParams = {
   navigation: FileBrowserScreenNavigationProp
 }
 
+function addDummyItems(items: ClowdFiles) {
+  const numColumns = 3
+  const dummyItemsNum = numColumns - (items.length % numColumns)
+  const temp = [...items]
+  for (let i = 0; i < dummyItemsNum; i++) {
+    temp.push({
+      title: '',
+      type: '',
+    })
+  }
+  return temp
+}
+
 const FileBrowserScreen: React.FC<FileBrowserScreenParams> = ({
   navigation,
 }) => {
   const numColumns = 3
-
-  function addDummyItems(items: ClowdFiles) {
-    const dummyItemsNum = numColumns - (items.length % numColumns)
-    const temp = [...items]
-    for (let i = 0; i < dummyItemsNum; i++) {
-      temp.push({
-        title: '',
-        type: '',
-      })
-    }
-    return temp
-  }
-
-  const [items, setItems] = useState(addDummyItems(sampleFiles))
-
-  function searchFiles(name: string) {
-    if (name.length === 0) {
-      setItems(addDummyItems(sampleFiles))
-      return
-    }
-
-    const newItems = sampleFiles.filter(item =>
-      item.title.toLocaleLowerCase().includes(name.toLocaleLowerCase())
-    )
-
-    setItems(addDummyItems(newItems))
-  }
-
-  let offset = 0
   const appContext = useContext(AppContext)
-
   const route = useRoute<FileBrowserScreenRouteProp>()
+  const pathName = route.params?.pathName || '/'
+  const [items, setItems] = useState([])
 
+  // Run once the screen has been loaded
   useEffect(() => {
     appContext.setNavigation(navigation)
     appContext.setIsNavShrunken(false)
-    console.log('screen loaded', appContext.scrollY)
+
     const navAnimation = Animated.timing(appContext.scrollY, {
       toValue: 0,
       duration: 300,
     })
     navAnimation.start()
+
+    const thisInfo = appContext.fileInfo.filter(info => {
+      const regExp = new RegExp(`${pathName}.*?[\.\/]`)
+      const matched = info.path.match(regExp)
+      return matched !== null
+    })
+
+    setItems(
+      addDummyItems(
+        thisInfo.map(info => {
+          const regExp = new RegExp(`${pathName}.*?[\.\/]`)
+          const matched = info.path.match(regExp)
+          let type = 'folder'
+          let title = ''
+          if (matched[0].endsWith('.')) {
+            type = info.path.replace(matched[0], '')
+            const splitted = info.path.split('/')
+            title = splitted[splitted.length - 1]
+          } else if (matched[0].endsWith('/')) {
+            // Folder
+            const splitted = matched[0].split('/')
+            title = splitted[splitted.length - 2]
+            type = 'folder'
+          }
+          return {
+            title,
+            type,
+          }
+        })
+      )
+    )
 
     let f: EventListenerCallback<'focus', undefined>
     navigation.addListener(
@@ -132,6 +147,7 @@ const FileBrowserScreen: React.FC<FileBrowserScreenParams> = ({
               if (item.type === 'folder') {
                 navigation.push('FileBrowser', {
                   folderName: item.title,
+                  pathName: pathName + item.title + '/',
                 })
               } else {
                 Share.share(
@@ -146,7 +162,7 @@ const FileBrowserScreen: React.FC<FileBrowserScreenParams> = ({
             }}
           />
         )}
-        keyExtractor={(item, index) => item.title}
+        keyExtractor={item => item.title}
       />
     </>
   )
