fec25a5e94828f8bf38cb970b1a7a788e84ebf66
Jang Haemin <io@jhaemin.com>
2020-02-25 00:38:14 -0500

    test(demo): unshift dummy files for demo

--- src/component/FileList.tsx
+++ src/component/FileList.tsx
@@ -12,6 +12,42 @@ import FileBrowserGridItem, {
 import { AppContext } from '@src/context/AppContext'
 import { getBottomSpace } from 'react-native-iphone-x-helper'
 
+function addDemoItems(items: ClowdFiles) {
+  const temp = [...items]
+  temp.unshift({
+    path: '/password.txt',
+    size: 0.01,
+    title: 'password.txt',
+    type: 'txt',
+  })
+  temp.unshift({
+    path: '/Clowd Logo.png',
+    size: 1.23,
+    title: 'Clowd Logo.png',
+    type: 'png',
+  })
+  temp.unshift({
+    path: '/Nicolas Cage.gif',
+    size: 8.61,
+    title: 'Nicolas Cage.gif',
+    type: 'gif',
+  })
+  temp.unshift({
+    path: '/New York.jpg',
+    size: 12.74,
+    title: 'New York.jpg',
+    type: 'jpg',
+  })
+  temp.unshift({
+    path: '/Purdue/.',
+    size: 0.52,
+    title: 'Purdue',
+    type: 'folder',
+  })
+
+  return temp
+}
+
 function addDummyItems(items: ClowdFiles) {
   const numColumns = 3
   const dummyItemsNum = numColumns - (items.length % numColumns)
@@ -47,44 +83,46 @@ const FileList: React.FC<FileBrowserStackScreenParams> = ({ navigation }) => {
 
     setItems(
       addDummyItems(
-        thisInfo
-          .map(info => {
-            const regExp = new RegExp(`${pathName}.*?[\.\/]`)
-            const matched = info.path.match(regExp)
-            let type = 'folder'
-            let title = ''
-            if (matched[0].endsWith('.')) {
-              if (matched[0].endsWith('/.')) {
-                // Current directory indicator "."
-              } else {
-                type = info.path.replace(matched[0], '')
-                const splitted = info.path.split('/')
-                title = splitted[splitted.length - 1]
+        addDemoItems(
+          thisInfo
+            .map(info => {
+              const regExp = new RegExp(`${pathName}.*?[\.\/]`)
+              const matched = info.path.match(regExp)
+              let type = 'folder'
+              let title = ''
+              if (matched[0].endsWith('.')) {
+                if (matched[0].endsWith('/.')) {
+                  // Current directory indicator "."
+                } else {
+                  type = info.path.replace(matched[0], '')
+                  const splitted = info.path.split('/')
+                  title = splitted[splitted.length - 1]
+                }
+              } else if (matched[0].endsWith('/')) {
+                // Folder
+                const splitted = matched[0].split('/')
+                title = splitted[splitted.length - 2]
+                type = 'folder'
               }
-            } else if (matched[0].endsWith('/')) {
-              // Folder
-              const splitted = matched[0].split('/')
-              title = splitted[splitted.length - 2]
-              type = 'folder'
-            }
-            return {
-              title,
-              type,
-              size: info.size,
-              path: info.path,
-            }
-          })
-          .filter((item, pos, self) => {
-            return (
-              self.findIndex(a => {
-                return (
-                  a.title !== '' &&
-                  a.title === item.title &&
-                  a.type === item.type
-                )
-              }) === pos
-            )
-          })
+              return {
+                title,
+                type,
+                size: info.size,
+                path: info.path,
+              }
+            })
+            .filter((item, pos, self) => {
+              return (
+                self.findIndex(a => {
+                  return (
+                    a.title !== '' &&
+                    a.title === item.title &&
+                    a.type === item.type
+                  )
+                }) === pos
+              )
+            })
+        )
       )
     )
   }, [appContext.fileInfo])
