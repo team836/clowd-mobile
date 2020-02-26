44947d1eea526617b15b26bc1411dd160f09b60a
Jang Haemin <io@jhaemin.com>
2020-02-25 22:39:08 -0500

    chore: update sample

--- src/data/sample-file-info.tsx
+++ src/data/sample-file-info.tsx
@@ -3,42 +3,109 @@ export type FileInfo = {
   size: number
 }
 
+// const sample: FileInfo[] = [
+//   {
+//     path: '/image.png',
+//     size: 12.0,
+//   },
+//   {
+//     path: '/photos/newyork.jpg',
+//     size: 4.2,
+//   },
+//   {
+//     path: '/photos/brooklyn-bridge.jpg',
+//     size: 13.7,
+//   },
+//   {
+//     path: '/photos/selfies/me.jpg',
+//     size: 8.61,
+//   },
+//   {
+//     path: '/icons/file-icon.png',
+//     size: 0.78,
+//   },
+//   {
+//     path: '/icons/clowd-icon.png',
+//     size: 0.52,
+//   },
+//   {
+//     path: '/texts/memo.txt',
+//     size: 0.012,
+//   },
+//   {
+//     path: '/robots.txt',
+//     size: 0.02,
+//   },
+//   {
+//     path: '/empty folder/.',
+//     size: 0,
+//   },
+// ]
+
 const sample: FileInfo[] = [
   {
-    path: '/image.png',
+    path: '/New York.jpg',
     size: 12.0,
   },
   {
-    path: '/photos/newyork.jpg',
-    size: 4.2,
+    path: '/Clowd Logo.png',
+    size: 1.2,
   },
   {
-    path: '/photos/brooklyn-bridge.jpg',
-    size: 13.7,
-  },
-  {
-    path: '/photos/selfies/me.jpg',
+    path: '/Nicolas Cage.gif',
     size: 8.61,
   },
   {
-    path: '/icons/file-icon.png',
-    size: 0.78,
+    path: '/note.txt',
+    size: 0.01,
   },
   {
-    path: '/icons/clowd-icon.png',
+    path: '/Purdue/.',
     size: 0.52,
   },
   {
-    path: '/texts/memo.txt',
+    path: '/CAU/.',
     size: 0.012,
   },
   {
-    path: '/robots.txt',
-    size: 0.02,
+    path: '/CAU2/.',
+    size: 0.012,
   },
   {
-    path: '/empty folder/.',
-    size: 0,
+    path: '/CAU3/.',
+    size: 0.012,
+  },
+  {
+    path: '/CAU4/.',
+    size: 0.012,
+  },
+  {
+    path: '/CAU5/.',
+    size: 0.012,
+  },
+  {
+    path: '/CAU6/.',
+    size: 0.012,
+  },
+  {
+    path: '/CAU7/.',
+    size: 0.012,
+  },
+  {
+    path: '/CAU8/.',
+    size: 0.012,
+  },
+  {
+    path: '/CAU9/.',
+    size: 0.012,
+  },
+  {
+    path: '/CAU10/.',
+    size: 0.012,
+  },
+  {
+    path: '/CAU11/.',
+    size: 0.012,
   },
 ]
 
