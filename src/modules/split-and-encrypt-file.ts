913fb69306f7b06e076a5f6dbd56e7afbe7e1c0c
Jang Haemin <io@jhaemin.com>
2020-02-25 22:40:16 -0500

    chore(test): disable encryption

--- src/modules/split-and-encrypt-file.ts
+++ src/modules/split-and-encrypt-file.ts
@@ -4,11 +4,11 @@ type Base64String = string
 
 const splitAndEcryptFile = function(file: Base64String) {
   const splittedPieces = file.match(/.{1,10485760}/g)
-  const encryptedPieces = splittedPieces.map(piece =>
-    CryptoJS.AES.encrypt(piece, 'clowd836').toString()
-  )
+  // const encryptedPieces = splittedPieces.map(piece =>
+  //   CryptoJS.AES.encrypt(piece, 'clowd836').toString(CryptoJS.enc.Base64)
+  // )
 
-  return encryptedPieces
+  return splittedPieces
 }
 
 export default splitAndEcryptFile
